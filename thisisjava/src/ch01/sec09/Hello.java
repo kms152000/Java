5dd90ff284d5abdba2f65702b3d11adbdb2a3a1b
kms15 <kms15@KMS>
2023-05-09 17:42:43 +0900

    첫 커밋

---
+++ thisisjava/src/ch01/sec09/Hello.java
@@ -0,0 +1,16 @@
+package ch01.sec09; // 파일 시스템에서는 (도트) 기준으로 상위 디렉토리와 하위 디렉토리로 생성된다.
+/**
+ * @author 김민석
+ */
+/*
+ 장제목: 1장 자바 시작하기
+ 작성일: 2023.05.09
+ */
+public class Hello { //클래스 블록
+	//프로그램 실행 진입점
+	public static void main(String[] args) { //메소드 븧혹
+		//콘솔에 출력하는 실행문
+		System.out.println("Hello, /* 주석 불가능 */ Java"); 
+	}
+
+}
