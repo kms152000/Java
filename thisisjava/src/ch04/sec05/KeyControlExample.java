1134e94b9cea574a8b3f012e8cabab0bc37b8828
Kms-office <kms15@KMS>
2023-05-31 17:58:28 +0900

    .

--- thisisjava/src/ch04/sec05/KeyControlExample.java
+++ thisisjava/src/ch04/sec05/KeyControlExample.java
@@ -1,10 +1,37 @@
 package ch04.sec05;
 
+import java.util.Scanner;
+
 public class KeyControlExample {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner scanner = new Scanner(System.in); //Scanner 생성
+		boolean run = true; //while 문의 조건식을 위한 변수 선언
+		int speed = 0;
+		
+		while(run) {
+			System.out.println("-------------------------");
+			System.out.println("1. 증속 | 2. 감속 | 3. 중지");
+			System.out.println("-------------------------");
+			System.out.print("선택: ");
+			
+			String strNum = scanner.nextLine(); //키보드에서 입력한 내용을 읽음.
+			System.out.println("");
+			
+			if(strNum.equals("1")) {
+				speed++;
+				System.out.println("현재 속도: " + speed);
+			}
+			if(strNum.equals("2")) {
+				speed--;
+				System.out.println("현재 속도: " + speed);
+			}
+			if(strNum.equals("3")) {
+				run = false;
+			}
+		}
+		
+		System.out.println("프로그램을 종료합니다.");
 	}
 
 }
---
+++ thisisjava/src/ch04/sec06/DoWhileExample.java
@@ -0,0 +1,9 @@
+package ch04.sec06;
+
+public class DoWhileExample {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
