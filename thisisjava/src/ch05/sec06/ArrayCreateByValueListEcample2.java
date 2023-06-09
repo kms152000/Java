65fd7bbde530db87220d29396a373e6e1a1b4224
Kms-office <kms15@KMS>
2023-06-09 17:17:59 +0900

    .

---
+++ thisisjava/src/ch05/sec06/ArrayCreateByValueListEcample2.java
@@ -0,0 +1,14 @@
+package ch05.sec06;
+
+public class ArrayCreateByValueListEcample2 {
+
+	public static void main(String[] args) {
+		//배열 변수 선언
+		int[] scores;
+		
+		//배열 변수에 배열을 대입
+		scores = new int[] { 83, 90, 87 };
+		//배열 항목의 총합을 구하고 출력
+	}
+
+}
