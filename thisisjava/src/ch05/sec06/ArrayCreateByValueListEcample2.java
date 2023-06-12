4f4c90f24ca9817de3ed29c1b5dcc4e8b06cdc1e
Kms-office <kms15@KMS>
2023-06-12 17:19:12 +0900

    .

--- thisisjava/src/ch05/sec06/ArrayCreateByValueListEcample2.java
+++ thisisjava/src/ch05/sec06/ArrayCreateByValueListEcample2.java
@@ -9,6 +9,16 @@ public class ArrayCreateByValueListEcample2 {
 		//배열 변수에 배열을 대입
 		scores = new int[] { 83, 90, 87 };
 		//배열 항목의 총합을 구하고 출력
+		int sum1 = 0;
+		for(int i=0; i<3; i++) {
+			sum1 += scores[i];
+		}
+		System.out.println("총합 : " + sum1);
+		
+		//배열을 매개값으로 주고, printItem() 메소드 호출
+		printItem( new int[] { 83, 90, 87 });
 	}
+	
+	//
 
 }
