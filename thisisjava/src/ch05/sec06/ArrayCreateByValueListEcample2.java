deaee50e2f6ad9080dc4db1f290467069663580f
Kms-office <kms15@KMS>
2023-06-14 17:38:38 +0900

    .

--- thisisjava/src/ch05/sec06/ArrayCreateByValueListEcample2.java
+++ thisisjava/src/ch05/sec06/ArrayCreateByValueListEcample2.java
@@ -19,6 +19,12 @@ public class ArrayCreateByValueListEcample2 {
 		printItem( new int[] { 83, 90, 87 });
 	}
 	
-	//
+	//printItem() 메소드 선언
+	public static void printItem( int[] scores ) {
+		//매개변수가 참조하는 배열의 항목을 출력
+		for(int i=0; i<3; i++) {
+			System.out.println("score["+i+"]: "+scores[i]);
+		}
+	}
 
 }
