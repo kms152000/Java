23f2ca93b79d4a1f2aa4447c64fdc4a9907f4df0
Kms-office <kms15@KMS>
2023-07-05 17:56:01 +0900

    .

---
+++ thisisjava/src/ch06/sec09/CarExample.java
@@ -0,0 +1,13 @@
+package ch06.sec09;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		Car pCar = new Car("포르쉐");
+		Car bCar = new Car("벤츠");
+		
+		pCar.run();
+		bCar.run();
+	}
+
+}
