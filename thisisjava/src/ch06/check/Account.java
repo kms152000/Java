8b5603c98fc4ec9845291548a7fe09f39ddeb994
Kms-office <kms15@KMS>
2023-07-12 17:59:44 +0900

    .

--- thisisjava/src/ch06/check/Account.java
+++ thisisjava/src/ch06/check/Account.java
@@ -1,17 +1,52 @@
 package ch06.check;
 
 public class Account {
-	public static final int MIN_BALANCE = 0;
-	public static final int MAX_BALANCE = 1000000;
+	//19.
+//	public static final int MIN_BALANCE = 0;
+//	public static final int MAX_BALANCE = 1000000;
+//	private int balance;
+//	
+//	public int getBalance() {
+//		return balance;
+//	}
+//	public void setBalance(int balance) {
+//		if(balance<Account.MIN_BALANCE || balance>Account.MAX_BALANCE){
+//			return;
+//		}
+//		this.balance = balance;
+//	}
+	private String ano;
+	private String owner;
 	private int balance;
 	
+	public Account (String ano, String owner, int balance) {
+		this.ano = ano;
+		this.owner = owner;
+		this.balance = balance;
+	}
+
+	public String getAno() {
+		return ano;
+	}
+
+	public void setAno(String ano) {
+		this.ano = ano;
+	}
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
 	public int getBalance() {
 		return balance;
 	}
+
 	public void setBalance(int balance) {
-		if(balance<Account.MIN_BALANCE || balance>Account.MAX_BALANCE){
-			return;
-		}
 		this.balance = balance;
 	}
+	
 }
---
+++ thisisjava/src/ch06/check/BankApplication.java
@@ -0,0 +1,7 @@
+package ch06.check;
+
+public class BankApplication {
+	private static Account[] accountArray = new Account[100];
+
+
+}
