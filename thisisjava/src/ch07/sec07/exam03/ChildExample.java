4669f552658063c24172de1bff8a62c7fad06f63
Kms-office <kms15@KMS>
2023-07-21 17:45:20 +0900

    .

--- thisisjava/src/ch07/sec07/exam03/ChildExample.java
+++ thisisjava/src/ch07/sec07/exam03/ChildExample.java
@@ -1,7 +1,7 @@
 package ch07.sec07.exam03;
 
 public class ChildExample {
-	public static void main(String args[]) {
+	public static void main(String[] args) {
 		//객체 생성 및 자동 타입 변환
 		Parent parent = new Child();
 		
