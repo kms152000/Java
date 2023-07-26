a50d561755b7ff41ecb2b3297878a71cce33613d
Kms-office <kms15@KMS>
2023-07-26 17:41:42 +0900

    .

--- thisisjava/src/ch07/sec10/exam01/Phone.java
+++ thisisjava/src/ch07/sec10/exam01/Phone.java
@@ -11,9 +11,9 @@ public class Phone {
 	
 	//메소드 선언
 	void turnOn() {
-		System.out.println("폰 전원을 켭니다.");
+		System.out.println(owner + "의 폰 전원을 켭니다.");
 	}
 	void turnOff() {
-		System.out.println("폰 전원을 끕니다.");
+		System.out.println(owner + "의 폰 전원을 끕니다.");
 	}
 }
--- thisisjava/src/ch07/sec10/exam01/SmartPhone.java
+++ thisisjava/src/ch07/sec10/exam01/SmartPhone.java
@@ -9,6 +9,6 @@ public class SmartPhone extends Phone{
 	
 	//메소드 선언
 	void internetSearch() {
-		System.out.println("인터넷 검색을 합니다.");
+		System.out.println(owner + "이(가) 인터넷 검색을 합니다.");
 	}
 }
