4357864b313b8e2a6ba4ec8429c6afcb3c312254
Kms-office <kms15@KMS>
2023-08-02 17:09:25 +0900

    .

--- thisisjava/src/ch08/sec05/RemoteControl.java
+++ thisisjava/src/ch08/sec05/RemoteControl.java
@@ -10,5 +10,14 @@ public interface RemoteControl {
 	void turnOff();
 	void setVolume(int volume);
 	
-	//
+	//디폴트 인스턴스 메소드
+	default void setMute(boolean mute) {
+		if(mute) {
+			System.out.println("무음 처리합니다.");
+			//추상 메소드 호출하면서 상수 필드 사용
+			setVolume(MIN_VOLUME);
+		} else {
+			System.out.println("무음 해제합니다.");
+		}
+	}
 }
