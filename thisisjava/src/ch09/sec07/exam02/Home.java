524277cdff5c43096ddc8b75bb4cd4e4ca2c92be
Kms-office <kms15@KMS>
2023-08-18 17:39:46 +0900

    .

--- thisisjava/src/ch09/sec07/exam02/Home.java
+++ thisisjava/src/ch09/sec07/exam02/Home.java
@@ -21,5 +21,26 @@ public class Home {
 	}
 	
 	//메소드(로컬 변수 이용)
+	public void use2() {
+		//로컬 변수에 익명 구현 객체 대입
+		RemoteControl rc = new RemoteControl() {
+			@Override
+			public void turnOn() {
+				System.out.println("에어컨을 켭니다.");
+			}
+			@Override
+			public void turnOff() {
+				System.out.println("에어컨을 끕니다.");
+			}
+		};
+		rc.turnOn();
+		rc.turnOff();
+	}
+	
+	//메소드(매개변수 이용)
+	public void use3(RemoteControl rc) {
+		rc.turnOn();
+		rc.turnOff();
+	}
 	
 }
