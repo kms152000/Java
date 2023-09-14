8470ee58454690062313fe30798f00963392fb38
YDPOfffice <kms152000@naver.com>
2023-09-14 18:00:57 +0900

    .

--- thisisjava/src/ch12/sec07/RandomExample.java
+++ thisisjava/src/ch12/sec07/RandomExample.java
@@ -18,9 +18,10 @@ public class RandomExample {
 		//당첨번호
 		int[] winningNumber = new int[6];
 		random = new Random(5);
-		System.out.println("당첨번호: ");
+		System.out.print("당첨번호: ");
 		for(int i=0; i<6; i++) {
 			winningNumber[i] = random.nextInt(45) + 1;
+			System.out.print(winningNumber[i] + " ");
 		}
 		System.out.println();
 		
@@ -28,6 +29,12 @@ public class RandomExample {
 		Arrays.sort(selectNumber);
 		Arrays.sort(winningNumber);
 		boolean result = Arrays.equals(selectNumber, winningNumber);
-		System.out.println();
+		System.out.print("당첨여부: ");
+		if(result) {
+			System.out.print("1등에 당첨되셨습니다.");
+		} else {
+			System.out.println("당첨되지 않았습니다.");
+		}
+		
 	}
 }
