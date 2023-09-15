3e3a727e7093e562a62e80a5f9edc5c2fd243b6f
YDPOfffice <kms152000@naver.com>
2023-09-15 18:02:09 +0900

    .

---
+++ thisisjava/src/ch12/sec08/LosAngelesExample.java
@@ -0,0 +1,16 @@
+package ch12.sec08;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class LosAngelesExample {
+	public static void main(String[] args) {
+		TimeZone timeZone = TimeZone.getTimeZone("America/Los_Angeles");
+		Calendar now = Calendar.getInstance( timeZone ); 
+		
+		int amPm = now.get(Calendar.AM_PM);
+		String strAmPm = null;
+		if
+		
+	}
+}
