de6b829e9a16dc9ca39cc75351439812e398dab6
YDPOfffice <kms152000@naver.com>
2023-09-18 17:57:27 +0900

    .

--- thisisjava/src/ch12/sec08/LosAngelesExample.java
+++ thisisjava/src/ch12/sec08/LosAngelesExample.java
@@ -10,7 +10,19 @@ public class LosAngelesExample {
 		
 		int amPm = now.get(Calendar.AM_PM);
 		String strAmPm = null;
-		if
+		if(amPm == Calendar.AM) {
+			strAmPm = "오전";
+		} else {
+			strAmPm = "오후";
+		}
+		int hour = now.get(Calendar.HOUR);
+		int minute = now.get(Calendar.MINUTE);
+		int second = now.get(Calendar.SECOND);
+		
+		System.out.print(strAmPm + " ");
+		System.out.print(hour + "시 ");
+		System.out.print(minute + "분 ");
+		System.out.println(second + "초");
 		
 	}
 }
