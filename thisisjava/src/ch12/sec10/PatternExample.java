4a382b8e6d98699f1d4d212763a523e7c3bce613
YDPOfffice <kms152000@naver.com>
2023-09-19 17:51:29 +0900

    .

--- thisisjava/src/ch12/sec10/PatternExample.java
+++ thisisjava/src/ch12/sec10/PatternExample.java
@@ -10,6 +10,17 @@ public class PatternExample {
 		boolean result = Pattern.matches(regExp, data);
 		if(result) {
 			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
+		}
+		
+		regExp = "\\w+@\\w+\\.\\w+(\\.\\w+)?";
+		data = "angel@mycompanycom";
+		result = Pattern.matches(regExp, data);
+		if(result) {
+			System.out.println("정규식과 일치합니다.");
+		} else {
+			System.out.println("정규식과 일치하지 않습니다.");
 		}
 	}
 
