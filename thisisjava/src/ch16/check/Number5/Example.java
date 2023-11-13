83942c4bb1d495145c5f866afdcdf3f9f9735c5c
YDPOfffice <kms152000@naver.com>
2023-11-13 17:58:52 +0900

    .

---
+++ thisisjava/src/ch16/check/Number5/Example.java
@@ -0,0 +1,13 @@
+package ch16.check.Number5;
+
+public class Example {
+	public static void main(String[] args) {
+		Button btnOk = new Button();
+		btnOk.setClickListener();
+		btnOk.click();
+		
+		Button btnOk = new Button();
+		btnOk.setClickListener();
+		btnOk.click();
+	}
+}
