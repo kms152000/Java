0b1d3f206de987171ae42704e18729f53b083e8d
YDPOfffice <kms152000@naver.com>
2023-11-21 18:00:34 +0900

    .

---
+++ thisisjava/src/ch17/sec06/exam02/MapExample.java
@@ -0,0 +1,12 @@
+package ch17.sec06.exam02;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class MapExample {
+	public static void main(String[] args) {
+		int[] intArray = {1,2,3,4,5};
+		
+		IntStream iS = Arrays.stream(intArray);
+	}
+}
