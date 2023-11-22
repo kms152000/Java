1c8a1c41c095ad7b67670b3edf6f4647c99502dc
YDPOfffice <kms152000@naver.com>
2023-11-22 17:50:25 +0900

    .

--- thisisjava/src/ch17/sec06/exam02/MapExample.java
+++ thisisjava/src/ch17/sec06/exam02/MapExample.java
@@ -8,5 +8,15 @@ public class MapExample {
 		int[] intArray = {1,2,3,4,5};
 		
 		IntStream iS = Arrays.stream(intArray);
+		iS
+			.asDoubleStream()
+			.forEach(d -> System.out.println(d));
+		
+		System.out.println();
+		
+		iS = Arrays.stream(intArray);
+		iS
+			.boxed()
+			.forEach(obj -> System.out.println(obj.intValue()));
 	}
 }
