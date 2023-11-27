6f9df629311a5d7216fa37bd8f0f9e821f1911f0
YDPOfffice <kms152000@naver.com>
2023-11-27 14:07:26 +0900

    .

--- thisisjava/src/ch17/sec10/OptionalExample.java
+++ thisisjava/src/ch17/sec10/OptionalExample.java
@@ -23,8 +23,22 @@ public class OptionalExample {
 		if (optional.isPresent()) {
 			System.out.println("방법1_평균: " + optional.getAsDouble());
 		} else {
-			System.out.println("방법1_평균: " + );
+			System.out.println("방법1_평균: 0.0");
 		}
+		
+		//방법2
+		double avg = list.stream()
+				.mapToInt(Integer :: intValue)
+				.average()
+				.orElse(0.0);
+		System.out.println("방법2_평균: " + avg);
+		
+		
+		//방법3
+		list.stream()
+			.mapToInt(Integer :: intValue)
+			.average()
+			.ifPresent(a -> System.out.println("방법3_평균: " + a));
 			
 	}
 }
